9e94c83b0ff6279703db7e99bb8ef9715b7023af
Arshia Naseri <arshianaseri7920@gmail.com>
2026-02-05 17:36:58 -0500

    Update .gitignore and enhance MyLlm functionality with online search and conversation history

--- My_LLM.py
+++ My_LLM.py
@@ -8,21 +8,24 @@ class MyLlm:
     conversation_history:list = None
     instructions:str = None
     Model:str = None
+    ModelTemperature = 0.7
     # Private
     __depth_search__ = 10
-    __LLM_CONFIG_INSTRUCTION__ = "Be concise. Answer in 1-3 sentences." # Things I want to set for my llm
+    __ONLINE_TRIGGER_LIST__ = ["weather", "news", "today", "current", "now", "latest", "price"] # If see these keywords, search online
+    __LLM_CONFIG_INSTRUCTION__ = "Be concise. Answer in 1-3 sentences" # Things I want to set for my llm
 
     def __init__(self, instructions:str = None, Model:str = "llama3.2"):
         self.Model = Model
-        self.instructions = instructions
+        self.instructions = instructions if instructions else ""
         self.conversation_history = [
             {
                 "role": "system",
-                "content": self.instructions + " " + self.__LLM_CONFIG_INSTRUCTION__
+                "content": f" {self.instructions}. {self.__LLM_CONFIG_INSTRUCTION__}"
             }
         ]
 
-    def web_search(self, query, max_results=__depth_search__):
+    def __web_search__(self, query, max_results=__depth_search__):
+        print("Searching Online ...")
         with DDGS() as ddgs:
             results = list(ddgs.text(query, max_results=max_results))
             return "\n".join([f"- {r['title']}: {r['body']}" for r in results])
@@ -30,6 +33,57 @@ class MyLlm:
     def clearChat(self):
         self.conversation_history = None
     
+    def chat(self, prompt:str, isStream = True, needFullConvo = False):
+        # Check if needs any online search
+        if self.__needOnlineSearch__(prompt):
+            search_results = self.__web_search__(prompt)
+            augmented_prompt = f"""User question: {prompt}
+                Web search results:
+                {search_results}
+                Answer based on the search results above."""
+            self.conversation_history.append({"role": "user", "content": augmented_prompt})
+        else:
+            self.conversation_history.append({"role": "user", "content": prompt})
+
+        data = {
+            "model": self.Model,
+            "messages": self.conversation_history, 
+            "options": {
+                "num_predict": 150,
+                "temperature": self.ModelTemperature
+            },
+            "stream": isStream
+        }
+
+        RES = requests.post(self.SERVER_URL, json=data, stream=isStream)
+        fullResponse = None
+        if isStream:
+            response = self.__chat_with_stream__(RES)
+        else:
+            response = self.__chat_without_stream__(RES)
+        
+        return response
+        
+
+    def __needOnlineSearch__(self, prompt:str):
+        return any(word in prompt.lower() for word in self.__ONLINE_TRIGGER_LIST__)
+    
+    def __chat_with_stream__(self, RES):
+        full_response = ""
+        for line in RES.iter_lines():
+            if line:
+                json_response = json.loads(line)
+                if 'message' in json_response:
+                    chunk = json_response['message']['content']
+                    full_response += chunk
+                    print(chunk, end="", flush=True)
+        print("")
+        return full_response
+
+    def __chat_without_stream__(self, RES):
+        return RES.json()['message']['content']
+    
+    # ! Requires string cleaning
     def __str__(self):
-        print("Chat History:")
-        print(self.conversation_history)
+        print(f"Chat History w/ Model {self.Model}")
+        return str(self.conversation_history)
--- local_llm_chatbot.py
+++ local_llm_chatbot.py
@@ -1,3 +1,14 @@
 from My_LLM import MyLlm
 
-myChat = MyLlm()
+
+if __name__ == "__main__":
+    beaverChat = MyLlm()
+    
+    while True:
+        prompt = input(">> ")
+        if prompt.lower() == '/exit':
+            break
+        
+        beaverChat.chat(prompt)
+    
+    print("=== LLM CLOSED ===")
